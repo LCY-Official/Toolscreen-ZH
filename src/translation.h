cbd7481e3f1141f466bc18d7fd2459d91d8ec67c
nineblooood <3501202786@qq.com>
2026-02-28 12:27:50 +0800

    introduce {fmt}

--- src/translation.h
+++ src/translation.h
@@ -6,4 +6,14 @@
 bool LoadTranslations(const std::string& lang);
 
 std::string tr(const char* key);
-std::string tr(const std::string& key);
+inline std::string tr(const std::string& key) { return tr(key.c_str()); }
+
+template <typename... Args>
+inline std::string ftr(const char* key, Args&&... args) {
+    return std::format(tr(key), std::forward<Args>(args)...);
+}
+
+template <typename... Args>
+inline std::string ftr(const std::string& key, Args&&... args) {
+    return ftr(key.c_str(), std::forward<Args>(args)...);
+}
